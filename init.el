5656bde3323c8a0b7ec01ed6b5c9aa50cea8302d
USAMI Kenta <tadsan@zonu.me>
2024-10-24 01:53:08 +0900

    Add init.el

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; init.el --- Config file for PoC of Editorconfig indentation issues  -*- lexical-binding: t; -*-
+
+;;; Code:
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name))
+  (setq package-user-dir (locate-user-emacs-file "elpa")))
+
+(package-initialize)
+(unless (require 'editorconfig nil t) (package-install 'editorconfig))
+(unless (require 'php-mode nil t) (package-install 'php-mode))
+
+;; Disable treesit
+(advice-add #'treesit-available-p :override (lambda () nil))
+
+(editorconfig-mode)
+;;; init.el ends here
